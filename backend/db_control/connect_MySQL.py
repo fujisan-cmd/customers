1898f630ca5bc301ca2f4349974e26bd816f0680
fujisan <fujiemasahiro88@gmail.com>
2025-02-10 21:49:27 +0900

    connect_MySQLwo

--- backend/db_control/connect_MySQL.py
+++ backend/db_control/connect_MySQL.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
 # 環境変数の読み込み
@@ -12,6 +13,7 @@ DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
+ssl_cert = str(Path(__file__).parents[1] / 'DigiCertGlobalRootCA.crt.pem')
 
 # MySQLのURL構築
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
@@ -21,5 +23,8 @@ engine = create_engine(
     DATABASE_URL,
     echo=True,
     pool_pre_ping=True,
-    pool_recycle=3600
+    pool_recycle=3600,
+    connect_args={
+        "ssl": {"ssl_ca": ssl_cert}
+    }
 )
